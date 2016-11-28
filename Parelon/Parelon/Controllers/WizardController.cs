cf6094aabe121c92f998cd87938a91c79da34502
gogolander <gogolander@gmail.com>
2016-11-28 05:09:34 +0100

    jQuery upgraded to 3.3.1 New wizard introduction page Created AddInitiative Support for attachments introduced Minor bugs and typos fixed

--- Parelon/Parelon/Controllers/WizardController.cs
+++ Parelon/Parelon/Controllers/WizardController.cs
@@ -8,13 +8,104 @@ namespace Parelon.Controllers
 {
     public class WizardController : Controller
     {
-        // GET: Wizard
-        public ActionResult Start()
+        public ActionResult Start ( )
         {
             if (!Request.IsAuthenticated)
                 return RedirectToAction("Login", "Account", new { ReturnUrl = "~/Wizard/Start" });
             else
                 return View();
+        }
+
+        [HttpGet]
+        public ActionResult AddInterrogation ( )
+        {
+            if (!Request.IsAuthenticated)
+                return RedirectToAction("Login", "Account", new { ReturnUrl = "~/Wizard/Start" });
+            else
+                return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public void AddInterrogation ( int i )
+        {
+        }
+
+        [HttpGet]
+        public ActionResult AddMotion ( )
+        {
+            if (!Request.IsAuthenticated)
+                return RedirectToAction("Login", "Account", new { ReturnUrl = "~/Wizard/Start" });
+            else
+                return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public void AddMotion ( int i )
+        {
+        }
+
+        [HttpGet]
+        public ActionResult AddBill ( )
+        {
+            if (!Request.IsAuthenticated)
+                return RedirectToAction("Login", "Account", new { ReturnUrl = "~/Wizard/Start" });
+            else
+                return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public void AddBill ( int i )
+        {
+        }
+
+        [HttpGet]
+        public ActionResult AddTemplate ( )
+        {
+            if (!Request.IsAuthenticated)
+                return RedirectToAction("Login", "Account", new { ReturnUrl = "~/Wizard/Start" });
+            else
+                return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public void AddTemplate ( int i )
+        {
+        }
+
+        [HttpGet]
+        public ActionResult AddIssue ( )
+        {
+            if (!Request.IsAuthenticated)
+                return RedirectToAction("Login", "Account", new { ReturnUrl = "~/Wizard/Start" });
+            else
+                return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public void AddIssue ( string problemTitle, string problemAbstract, string problemDescription,
+            string problemAim, string problemKeywords, string problemPolicy )
+        {
+        }
+
+        [HttpGet]
+        public ActionResult AddInitiative ( )
+        {
+            if (!Request.IsAuthenticated)
+                return RedirectToAction("Login", "Account", new { ReturnUrl = "~/Wizard/Start" });
+            else
+                return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public void AddInitiative ( string problemTitle, string problemAbstract, string problemDescription,
+            string problemAim, string problemKeywords, string problemPolicy )
+        {
         }
     }
 }
